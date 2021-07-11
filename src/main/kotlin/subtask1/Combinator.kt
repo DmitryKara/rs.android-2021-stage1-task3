cd195e66cac26f2f35217787eeae5e334de11a77
DmitryKara <13kara.kara13@gmail.com>
2021-07-11 16:51:36 +0300

    SuqareDecomposer - Finished

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -6,4 +6,7 @@ class Combinator {
     fun checkChooseFromArray(array: Array<Int>): Int? {
         throw NotImplementedError("Not implemented")
     }
-}
+
+   }
+
+
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,34 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+        val sqrNumber = number * number
+        return decomposer(sqrNumber, number - 1)?.toTypedArray()
+    }
+    private fun decomposer(number: Int, remainder: Int): ArrayList<Int>?{
+        for (i in remainder downTo 1){
+            val square = i * i
+             if (number - square == 0){
+                 return arrayListOf(i)
+             }
+             if (number - square < 0){
+                 return null
+             }
+            var newRemainder = sqrt((number - square).toDouble()).toInt()
+            if (newRemainder >= i){
+                newRemainder = i - 1
+            }
+            val result = decomposer(number - square, newRemainder)
+            if (result != null){
+                result.add(i)
+                return result
+            }
+        }
+        return null
     }
 }
