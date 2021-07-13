8078a26c2870280d65a4ec9a3e5ca1c8da4c8dfd
DmitryKara <13kara.kara13@gmail.com>
2021-07-14 01:00:10 +0300

    Combinator finished

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,9 +4,32 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        var result: Int? = null
+        val n = array[1]
+        val m = array[0]
+
+
+        for (x in 1..n) {
+            val c = factorial(n) / (factorial(x) * factorial(n - x))
+            if (c.toInt() == m ) {
+                result = x
+                break
+            }
+        }
+        return result
+    }
+     fun factorial(n: Int): Long{
+        return if (n < 2) 1
+        else n * factorial(n - 1)
     }
 
    }
+// n!=n(n−1)!=n(n−1)(n−2)!=n(n−1)(n−2)(n−3)...(n-x)!
+// m = !n / !x * (n - x)!
+// m - количество плакатов
+// n - общее количетво цветов
+// x - количество цветов для одного плаката ??
+// x! = n(n−1)(n−2)(n−3)...(n-x-1) / m
 
 
